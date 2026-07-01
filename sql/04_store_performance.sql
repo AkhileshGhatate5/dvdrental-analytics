da0101616051a9bef8b8f741de3338a20ae1385c
Akhilesh Ghatate <akhileshghatate28@gmail.com>
2026-07-01 16:20:00 +0530

    Update 04_store_performance.sql
    Added a SQL query to analyze store performance based on revenue and rental volume.

--- sql/04_store_performance.sql
+++ sql/04_store_performance.sql
@@ -1,1 +1,14 @@
+-- Business Question 4: Which store performs better — revenue & rental volume?
 
+
+select store.store_id,
+  count(rental.rental_id),
+	sum(amount) from store
+left join inventory
+	on store.store_id=inventory.store_id
+left join rental
+	on inventory.inventory_id=rental.inventory_id
+left join payment
+	on payment.rental_id=rental.rental_id
+group by store.store_id
+order by sum(amount) desc
