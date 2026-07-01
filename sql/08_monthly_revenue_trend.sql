c943ff65122bff76e76b687a69efe8130b7dda81
Akhilesh Ghatate <akhileshghatate28@gmail.com>
2026-07-01 21:27:16 +0530

    Update 08_monthly_revenue_trend.sql

--- sql/08_monthly_revenue_trend.sql
+++ sql/08_monthly_revenue_trend.sql
@@ -1,1 +1,8 @@
+-- Business Question 08: Revenue trend over time (monthly)
 
+
+select round(SUM(amount),2) as total_revenue,
+to_char(payment_date,'YYYY-Month') as Monthly
+from payment
+group by to_char(payment_date,'YYYY-Month')
+order by round(SUM(amount),2) desc
