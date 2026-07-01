f3eb44dc29e3945143c3e72b0f12ae61d3bd1fdf
Akhilesh Ghatate <akhileshghatate28@gmail.com>
2026-07-01 14:56:17 +0530

    Update 01_revenue_by_category.sql

--- sql/01_revenue_by_category.sql
+++ sql/01_revenue_by_category.sql
@@ -1,13 +1,14 @@
 -- Business Question 1: Which film categories generate the most revenue?
 
-SELECT 
-    c.name AS category,
-    SUM(p.amount) AS total_revenue
-FROM payment p
-JOIN rental r ON p.rental_id = r.rental_id
-JOIN inventory i ON r.inventory_id = i.inventory_id
-JOIN film f ON i.film_id = f.film_id
-JOIN film_category fc ON f.film_id = fc.film_id
-JOIN category c ON fc.category_id = c.category_id
-GROUP BY c.name
-ORDER BY total_revenue DESC;
+select SUM(p.amount) as total_revenue,
+    category.name as category from category
+left join film_category
+	on category.category_id = film_category.category_id
+left join inventory
+	on film_category.film_id=inventory.film_id
+left join rental
+	on inventory.inventory_id=rental.inventory_id
+left join payment as p
+	on rental.rental_id=p.rental_id
+group by category.name
+order by  SUM(p.amount) DESC
