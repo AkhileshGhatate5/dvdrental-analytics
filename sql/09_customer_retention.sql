b405ffc8bef4b163b7a7eaf08f6e183b0961cc76
Akhilesh Ghatate <akhileshghatate28@gmail.com>
2026-07-01 22:33:31 +0530

    Update 09_customer_retention.sql
    Add SQL query to analyze customer retention based on rental occurrences.

--- sql/09_customer_retention.sql
+++ sql/09_customer_retention.sql
@@ -1,1 +1,13 @@
+-- Business Question 09: Customer retention — one-time vs repeat renters?
 
+select customer.customer_id,
+	   count(rental.rental_id) as rental_occurrence,
+		case
+			when count(rental.rental_id) = 1 then 'one-time'
+			else 'repeat'
+		end as customer_type
+from customer
+left join rental on
+customer.customer_id = rental.customer_id
+group by customer.customer_id
+order by count(rental.rental_id)
