3676a7016f7834a32496d88b6849f447a049fe08
Akhilesh Ghatate <akhileshghatate28@gmail.com>
2026-07-02 16:11:34 +0530

    Update 11_revenue_by_city.sql
    Added SQL query to calculate highest rental revenue by city.

--- sql/11_revenue_by_city.sql
+++ sql/11_revenue_by_city.sql
@@ -1,1 +1,13 @@
+--Business Question 11: Which cities generate the highest rental revenue?
 
+select city,
+	   sum(amount) as highest_rental_revenue
+	   from customer
+left join address on
+	customer.address_id = address.address_id
+left join city on
+	address.city_id = city.city_id
+left join payment on
+	customer.customer_id = payment.customer_id
+group by city
+order by sum(amount) desc
