dda822d9cf0bdb77075a56dcdb8d767ab70a4008
Akhilesh Ghatate <akhileshghatate28@gmail.com>
2026-07-02 15:38:59 +0530

    Update 10_rentals_by_film_rating.sql
    Added SQL query to analyze rentals by film rating.

--- sql/10_rentals_by_film_rating.sql
+++ sql/10_rentals_by_film_rating.sql
@@ -1,1 +1,11 @@
+--Business Question 10:Which film rating (G, PG, R, etc.) drives the most rentals?
 
+select rating,
+	count(rental_id) as total_rentals
+	from film
+left join inventory on
+	film.film_id = inventory.film_id
+left join rental on
+	inventory.inventory_id = rental.inventory_id
+group by rating
+order by count(rental_id) desc
